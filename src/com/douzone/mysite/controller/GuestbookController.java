09c8200229df949613be0b382081fca18ecd2b61
user <user@DESKTOP-KE0OSEE>
2020-02-09 23:58:50 +0900

    GuestbookController 주석 제거 및 코드 정리

--- src/com/douzone/mysite/controller/GuestbookController.java
+++ src/com/douzone/mysite/controller/GuestbookController.java
@@ -16,61 +16,9 @@ public class GuestbookController extends HttpServlet {
 		
 		String actionName = request.getParameter("a");
 		
-		//if(request.getParameter("a")==null) {actionName = " ";}// 해당 줄 없으면 오류... 도대체 뭐가 문제일까? // null처리를 
-		
 		Action action = new GuestbookActionFactory().getAction(actionName);
 		action.execute(request,response);
-
-		/*
-		if("deleteform".equals(action)) {
-
-			WebUtil.forward("/WEB-INF/views/guestbook/deleteform.jsp", request, response);
-
-		} else if("delete".equals(action)) {
-
-			String no = request.getParameter("no");
-			String password = request.getParameter("password");
-
-			Long noLong = Long.parseLong(no);
-
-			GuestbookDao dao = new GuestbookDao();
-
-			if(dao.ckPassword(noLong, password)){
-				dao.delete(noLong);
-			}
-			else{
-
-			}
-
-			WebUtil.redirect(request.getContextPath()+"/guestbook", request, response);
-			// response.sendRedirect(request.getContextPath()+"/guestbook");
-
-		} else if("insert".equals(action)) {
-			String name = request.getParameter("name");
-			String pass = request.getParameter("pass");
-			String content = request.getParameter("content");
-
-			GuestbookVo vo = new GuestbookVo();
-
-			vo.setName(name);
-			vo.setPassword(pass);
-			vo.setContents(content);
-
-			if(!(name.isEmpty()||pass.isEmpty()||content.isEmpty())){
-				new GuestbookDao().insert(vo);
-			}
-			WebUtil.redirect(request.getContextPath()+"/guestbook", request, response);
-			// response.sendRedirect(request.getContextPath()+"/guestbook");
-
-		} else {
-			// default 요청처리
-			List<GuestbookVo> list = new GuestbookDao().findAll();
-
-			request.setAttribute("list", list);
-
-			WebUtil.forward("/WEB-INF/views/guestbook/list.jsp", request, response);
-		}
-		 */
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
